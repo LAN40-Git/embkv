5e135a7d2e683e2b3c956f86ef95a9c0d5df858e
LAN40 <sdfk32@outlook.com>
2025-06-27 16:32:33 +0800

    修正节点集群构造中的节点名称，确保一致性和正确性

--- embkv/src/main.cpp
+++ embkv/src/main.cpp
@@ -5,10 +5,12 @@ using namespace embkv::raft;
 int main() {
     // 构造节点集群
     std::unordered_map<uint64_t, std::unique_ptr<Peer>> peers;
-    peers.emplace(0, std::make_unique<Peer>(0, 0, "Node1", "127.0.0.0", 8080));
-    peers.emplace(1, std::make_unique<Peer>(0, 1, "Node2", "127.0.0.0", 8081));
-    peers.emplace(2, std::make_unique<Peer>(0, 2, "Node3", "127.0.0.0", 8081));
+    peers.emplace(0, std::make_unique<Peer>(0, 0, "Node0", "127.0.0.0", 8080));
+    peers.emplace(1, std::make_unique<Peer>(0, 1, "Node1", "127.0.0.0", 8081));
+    peers.emplace(2, std::make_unique<Peer>(0, 2, "Node2", "127.0.0.0", 8081));
     auto transport = std::make_shared<Transport>(std::move(peers));
     transport->run();
+    RaftNode node(transport);
+    node.run();
     while (true) { std::this_thread::sleep_for(std::chrono::milliseconds(10000)); }
 }
