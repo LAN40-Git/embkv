4c7217d7ea0a6984c4c9066c1dafa9c343fefff7
LAN <sdfk32@outlook.com>
2025-07-03 14:20:55 +0800

    111

--- toolchain-arm.cmake
+++ toolchain-arm.cmake
@@ -1,15 +1,22 @@
-# arm-toolchain.cmake
-set(CMAKE_SYSTEM_NAME Linux)
+
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_C_COMPILER "/usr/bin/arm-linux-gnueabihf-gcc")
-set(CMAKE_CXX_COMPILER "/usr/bin/arm-linux-gnueabihf-g++")
+# 指定编译器
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+set(SQLite3_INCLUDE_DIR "/usr/arm-linux-gnueabihf/include")
+set(SQLite3_LIBRARY "/usr/arm-linux-gnueabihf/lib/libsqlite3.so")
 
+# 设置查找路径
 set(CMAKE_FIND_ROOT_PATH
-    /home/hujinhao/arm-boost
     /usr/arm-linux-gnueabihf
+    /home/${USER}/arm-libs
 )
 
+# 禁用主机路径查找
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# 抑制警告
+add_compile_options(-Wno-psabi)
